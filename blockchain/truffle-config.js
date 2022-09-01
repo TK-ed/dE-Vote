410d57d84e010ea44dd2049401d134282378f9e6
arjunarj <nagarjunan66@gmail.com>
2022-09-01 21:02:01 +0530

    Initial commit

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    networks: {
+      development: {
+        host: "127.0.0.1",
+        port: 8545,
+        network_id: "*" // Match any network id
+      },
+    },
+    contracts_directory: './src/contracts/',
+    contracts_build_directory: './src/build/',
+    compilers: {
+      solc: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+      version: "0.5.19"
+      }
+    }
+}
